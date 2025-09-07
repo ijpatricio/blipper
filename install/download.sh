7c0fe68a79b72b14baa58b500bef924a69d088f5
Patricio <ijpatricio@gmail.com>
2025-09-07 20:38:37 +0100

    wip

--- install/download.sh
+++ install/download.sh
@@ -11,5 +11,8 @@ cp .env.example .env
 echo "┌─────────────────────────────────────────────┐"
 echo "│  ⚠️  IMPORTANT: Change user and password    │"
 echo "│  in the .env file                           │"
+echo "│                                             |"
+echo "│  Run: nano .env                             |"
+echo "│                                             |"
 echo "│  Then run: ./install/start.sh               │"
 echo "└─────────────────────────────────────────────┘"
